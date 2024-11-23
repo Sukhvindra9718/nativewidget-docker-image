88e6155bc7a217174846fbe93fbb61837ac19a82
Sukhvindra9718 <sukhvindra.singh@resilientitservices.com>
2024-11-23 15:51:24 +0530

    project added

---
+++ project/theme/native/main.js
@@ -0,0 +1,19 @@
+export {};
+
+
+export const DynamicImage = {
+    container:{
+        borderWidth:1,
+        borderColor:"grey",
+        borderRadius:10,
+        width:115,
+        height:70,
+        marginBottom:10
+    },
+    image:{
+        height:"100%",
+        width:"100%",
+        borderRadius:10,
+        resizeMode:"contain"
+    }
+}
